bdfc8be763fb822b0c5aeb169ba6afd65b58c95c
xxwei <13168796131@163.com>
2016-02-26 14:21:06 +0800

    xxw1 创建 Person 类

---
+++ GitDemo1/GitDemo1/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GitDemo1
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 XuXiaoWei. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
