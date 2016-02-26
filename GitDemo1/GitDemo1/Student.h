e4f2248f11d5bd4f1600d7fc12dcecddf58b7221
xxwei <13168796131@163.com>
2016-02-26 14:25:31 +0800

    xxw2 创建一个Student 类

---
+++ GitDemo1/GitDemo1/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GitDemo1
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 XuXiaoWei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ GitDemo1/GitDemo1/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GitDemo1
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 XuXiaoWei. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
